6d7d3eae1ad172f6f2014685e1b9923741e7529b
gy991007 <54530838+gy991007@users.noreply.github.com>
2020-03-24 09:32:07 +0800

    palyer-add finished by gaoyan

---
+++ group2/Scripts/PhotonTextView.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonTextView : MonoBehaviour, IPunObservable
+{
+
+    #region private field
+    Text mytext;
+    #endregion
+    #region IPunObservable implementation
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(mytext.text);
+        }
+        else
+        {
+            mytext.text=(string)stream.ReceiveNext();
+        }
+    }
+
+    #endregion
+    #region MonoBehaviour CallBack
+    private void Start()
+    {
+        mytext = gameObject.GetComponent<Text>();
+    }
+    #endregion 
+}
