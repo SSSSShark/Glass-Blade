ac5118c38a78ef25757144290e8acb27a4239824
剑 <wangmingjian0607@163.com>
2020-03-25 21:01:50 +0800

    add joystick

---
+++ group1/Scripts/Joystick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public Transform handle;
+    public RectTransform panel;
+    public int maxradius;
+    public Vector2 Movement { get => movement; }
+
+    private Vector2 movement;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //可能需要区分pointerID
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(panel, eventData.position, eventData.pressEventCamera, out Vector2 position);
+        position = position.normalized * (position.magnitude > maxradius ? maxradius : position.magnitude);
+        movement = position / maxradius;
+        handle.localPosition = position;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        movement = handle.localPosition = Vector2.zero;
+    }
+}
