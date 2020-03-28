ba5704a7cb3a57d56c0d77e19d2231bfddc87029
lichenhao1 <lichenhao@mail.ustc.edu.cn>
2020-03-29 00:13:40 +0800

    lichenhao update

---
+++ group2/Scripts/DestroyWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWeapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+  void Start()
+  {
+    // 武器攻击画面消失
+    GameObject.Destroy(gameObject, 1.0f);
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+        
+  }
+}
