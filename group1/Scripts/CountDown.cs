aa49ef816de8f47ec640df2f3a7993889cd3f05a
David Wang <wrs792141579@outlook.com>
2020-04-02 01:50:56 +0800

    CountDown && ScoreBoard

---
+++ group1/Scripts/CountDown.cs
@@ -0,0 +1,35 @@
+﻿//Author: David Wang
+//Function: To show countdown time, component of Canvas/Information/CountDown
+//Todo:Add Canvas to the scene, then add Information.prefab,add this script in CountDown
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public int minute;
+    private int getTime;
+    private float timer = 0;
+    public Text countTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        getTime = 60 * minute;
+        countTimeText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int M = getTime / 60;
+        int S = getTime % 60;
+        timer += Time.deltaTime;
+        if(timer >= 1f)
+        {
+            timer = 0;
+            getTime --;
+            countTimeText.text = M + ":" + string.Format("{0:00}",S);
+        }
+    }
+}
---
+++ group1/Scripts/ScoreIncrease.cs
@@ -0,0 +1,33 @@
+﻿//Author: David Wang
+//Function: For group2: When a player dies, his opponent score increases by 1, shown on the scoreboard
+//Todo: After doing what is mentioned in CountDown.cs, add this script to Score A & Score B
+//Notion: UpdateScore function is to be called
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreIncrease : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void UpdateScore()
+    {
+        score ++;
+        if(score >= 10)
+        {
+            scoreText.text = score.ToString();
+        }
+        else
+        {
+            scoreText.text = "0" + score.ToString();       
+        }
+    }
+}
