42cdcc74e8baae860284e5c6fa23a40aad02dc19
PB17111661 <55393352+PB17111661@users.noreply.github.com>
2020-04-20 01:35:41 +0800

    work week8
    场景转换和基本占点逻辑

---
+++ group1/demo_week8/Assets/group1/Scripts/Occupy/OPlayerCharacter.cs
@@ -0,0 +1,14 @@
+﻿// Author: David Wang
+// Just for test!
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Glassblade.Group1
+{
+    public class OPlayerCharacter : MonoBehaviour
+    {
+        public bool isAlive=true;
+    }
+}
